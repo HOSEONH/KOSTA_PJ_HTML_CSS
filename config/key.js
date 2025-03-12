43988474944649c9af6228ed25a1331edcdf7103
황호선 <3262398@naver.com>
2025-03-12 21:38:04 +0900

    환경변수 수정

--- config/key.js
+++ config/key.js
@@ -1,6 +1,6 @@
 const config = {
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  supabaseUrl: window.env.SUPABASE_URL,
+  supabaseAnonKey: window.env.SUPABASE_ANON_KEY,
 };
 
 export default config;
